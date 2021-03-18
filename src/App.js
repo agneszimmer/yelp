9f5953c0aec92192569915f13b82a0d8aa438c07
Helder Vieira <helder.zv@gmail.com>
2021-03-18 16:34:16 +0100

    homepage + nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <React.Fragment>
+        <Switch>
+          
+          <Route exact path="/" render={ ()=> <Homepage /> } />
+         {/*  <Route exact path="/details/:id" render={ (props)=> <Details {...props} /> } /> */}
+          <Route exact path= "/>city" component={City}/>
+          <Route path="*" component={PageNotFound} />
 
+        </Switch>
+
+      </React.Fragment>
     </div>
   );
 }
